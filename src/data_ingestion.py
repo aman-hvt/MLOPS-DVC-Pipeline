eeeac6d593e24f4df11e01305ecf62fecff91e8e
Aman Tyagi <aman.tyagi@hexaviewtech.com>
2026-04-08 10:32:32 +0530

    Param changes

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 import logging
+import yaml
 
 # ensure Log dire present or nedd to create
 log_dir = 'Logs'
@@ -25,6 +26,23 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 def load_data(data_url: str)-> pd.DataFrame:
     try:
@@ -65,12 +83,15 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        data_size = 0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+        # test_size = 0.2
+        # data_size = 0.2
         # data_path = r"C:\Users\aman.tyagi\Desktop\MLOPS-DVC-Pipeline\experiments\spam.csv"
         data_path = "https://raw.githubusercontent.com/vikashishere/YT-MLOPS-Complete-ML-Pipeline/refs/heads/main/experiments/spam.csv"
         df =load_data(data_path)
         final_df = preprocess_data(df)
-        train_data, test_data = train_test_split(final_df, test_size=data_size, random_state=2)
+        train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=2)
         save_data(train_data,test_data,data_path = "./data")
     except Exception as e:
         logger.error("An error occured in main function: %s",e)
